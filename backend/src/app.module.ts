0a8ca90abe56a74734a8c7774f4c95568c3d0eb0
Kazutomo Deguchi <kazutomo.deguchi@shinagawa-web.com>
2025-04-24 09:21:51 +0900

    add posts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ['.env'],
     }),
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common/interfaces/hooks';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect(); // アプリ起動時にDB接続
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect(); // アプリ終了時に切断
+  }
+}
