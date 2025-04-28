ff456c88164d5e582eabc1ab44f03cd2cc4f316b
Kazutomo Deguchi <kazutomo.deguchi@shinagawa-web.com>
2025-04-28 16:11:16 +0900

    fix env.test

--- backend/src/config/dotenv/unit-test-config.ts
+++ backend/src/config/dotenv/unit-test-config.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 const testEnv = dotenv.config({
-  path: path.join(process.cwd(), '.env.test'),
+  path: path.join(process.cwd(), '.env.test.local'),
 });
 
 Object.assign(process.env, {
