c8dfe3bde8a28ec3d9f6e9df29eff27093ad2ab8
Kazutomo Deguchi <kazutomo.deguchi@shinagawa-web.com>
2025-04-29 11:54:34 +0900

    add console.log

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -10,6 +10,7 @@ export function PostList() {
   const [posts, setPosts] = useState<Post[]>([]);
 
   async function fetchPosts() {
+    console.log('VITE_API_URL',import.meta.env.VITE_API_URL)
     const response = await fetch(`${import.meta.env.VITE_API_URL}/posts`);
     const data = await response.json();
     setPosts(data);
@@ -23,7 +24,7 @@ export function PostList() {
     <div className="mt-12 w-full max-w-2xl">
       <h2 className="text-xl font-bold mb-4 text-blue-700">投稿一覧</h2>
       <ul className="space-y-4">
-        {posts.map((post) => (
+        {posts?.map((post) => (
           <li key={post.id} className="p-4 bg-white rounded shadow">
             <h3 className="text-lg font-semibold">{post.title}</h3>
             {post.content && <p className="mt-2 text-gray-600">{post.content}</p>}
